a546fcb1d50c629f06546a32bb6d3905730ae63b
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-14 15:57:40 +0200

    Good orientation gotta adjust the centering and the animation

--- SortGraph.c
+++ SortGraph.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <time.h>
 
 #define WIDTH 1200
 #define HEIGHT 600
@@ -7,8 +8,23 @@
 #define LEN_LIST 20
 #define LIST_MAX 100
 
-int l_value =((WIDTH - (LEN_LIST + 2)) / LEN_LIST);
-int values[LEN_LIST] = {17,92,27,63,71,87,42,26,01,52,65,96,61,28,36,14,75,46,56,15};
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+void wait( int seconds )
+{   // Pretty crossplatform, both ALL POSIX compliant systems AND Windows
+    #ifdef _WIN32
+        Sleep( 1000 * seconds );
+    #else
+        sleep( seconds );
+    #endif
+}
+
+int l_value =((WIDTH /(LEN_LIST + 5 )));
+int values[LEN_LIST] = {17,100,27,63,71,87,42,26,01,52,65,96,61,28,36,14,75,46,56,15};
 Vector4 past_values[20];
 
 int main(int argc, char *argv[]) {
@@ -17,29 +33,31 @@ int main(int argc, char *argv[]) {
 
   while(!WindowShouldClose()) {
     int prev_value = 0;
-    Vector2 start = (Vector2) {1,HEIGHT / 5 - LIST_MAX};
+    Vector2 start = (Vector2) {5,HEIGHT - 5};
     BeginDrawing();
-    ClearBackground(BLACK);
     for (int i=0; i<LEN_LIST; i++ ) {
-      if(values[i] <=prev_value && prev_value!= 0) {
+      if(values[i] <= prev_value && prev_value!= 0) {
         values[i-1] = values[i];
         values[i] = prev_value;
       }
 
       prev_value = values[i];
-      Vector2 end = (Vector2) {start.x, values[i] *HEIGHT / LIST_MAX};
+      Vector2 end = (Vector2) {start.x, HEIGHT - (values[i] *(HEIGHT / LIST_MAX))};
       DrawLineEx(start,end,l_value ,GREEN);
-      start.x += l_value + 1;
-      Vector2 start2 = (Vector2) {1,HEIGHT - LIST_MAX/5};
+      
+      start.x += l_value + 5;
+      Vector2 start2 = (Vector2) {5,HEIGHT - 5};
       for (int i = 0; i<LEN_LIST; i++) {
-        Vector2 end = (Vector2) {start2.x, values[i] *HEIGHT / LIST_MAX};
+        Vector2 end = (Vector2) {start2.x, HEIGHT - (values[i] *(HEIGHT / LIST_MAX))};
         DrawLineEx(start2,end,l_value ,GRAY);
-        start2.x += l_value + 1;
+        start2.x += l_value + 5;
 
       }
+      //sleep(1);
+      ClearBackground(BLACK);
     }
-    printf("\ninit\n");
-    for (int i = 0; i < 20; i++) printf("list: %d", values[i]);
+    //printf("\ninit\n");
+    //for (int i = 0; i < 20; i++) printf("list: %d", values[i]);
     EndDrawing();
   }
   return 0;
